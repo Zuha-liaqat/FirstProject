77b4fff1185b8132f3d94c1ccd513db9d7cb20ad
Zuha-liaqat <zuhaliaqat996@gmail.com>
2025-09-08 16:52:04 +0500

    Changes

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -32,7 +32,7 @@ useEffect(() => {
     }),
     onSubmit: async (values, { resetForm }) => {
       try {
-        const res = await fetch(`http://localhost:3000/api/v1/auth/login`, {
+        const res = await fetch(`${API_URL}/auth/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           
